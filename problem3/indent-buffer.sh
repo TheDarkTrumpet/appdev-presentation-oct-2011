093ae505c1da1dedb2d086d7e749fd6f66208129
David Thole <dthole@gmail.com>
2011-10-29 22:17:08 -0500

    Updating the shell script to the final version

--- problem3/indent-buffer.sh
+++ problem3/indent-buffer.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/emacs --script
-
-(setq file-name (car command-line-args-left))
-
-(setq myb (find-file file-name))
-;(set-buffer-major-mode myb 'php-mode)
+(find-file (car command-line-args-left))
 (indent-region (point-min) (point-max) nil)
 (save-buffer)
